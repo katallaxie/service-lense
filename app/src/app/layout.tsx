36f82622faa89d02a7302a9d087b9c3d24dc8f12
Sebastian Döll <sebastian@katallaxie.me>
2023-11-14 08:36:48 +0000

    wip: refactor the layout

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,5 +1,11 @@
 import ThemeProvider from '@/theme'
 import '@/styles/globals.css'
+import Image from 'next/image'
+
+import { MainNav } from '@/components/main-nav'
+import { Search } from '@/components/search'
+import TeamSwitcher from '@/components/team-switcher'
+import { UserNav } from '@/components/user-nav'
 
 export default function RootLayout({
   children
@@ -16,7 +22,35 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <div className="md:hidden">
+            <Image
+              src="/examples/dashboard-light.png"
+              width={1280}
+              height={866}
+              alt="Dashboard"
+              className="block dark:hidden"
+            />
+            <Image
+              src="/examples/dashboard-dark.png"
+              width={1280}
+              height={866}
+              alt="Dashboard"
+              className="hidden dark:block"
+            />
+          </div>
+          <div className="hidden flex-col md:flex">
+            <div className="border-b">
+              <div className="flex h-16 items-center px-4">
+                <TeamSwitcher />
+                <MainNav className="mx-6" />
+                <div className="ml-auto flex items-center space-x-4">
+                  <Search />
+                  <UserNav />
+                </div>
+              </div>
+            </div>
+            <div className="flex-1 space-y-4 p-8 pt-6">{children}</div>
+          </div>
         </ThemeProvider>
       </body>
     </html>
