d2e2ae5c57cc81cb88fd40aa4bc0484f11457281
Sebastian Döll <sebastian@katallaxie.me>
2023-12-11 21:15:46 +0000

    wip: layout the lens

--- src/app/dashboard/workloads/[id]/lenses/[lensId]/layout.tsx
+++ src/app/dashboard/workloads/[id]/lenses/[lensId]/layout.tsx
@@ -47,10 +47,10 @@ export default async function Layout({ params, children }: LayoutProps) {
       </SubNav>
       <main className="p-8">
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/5">
+          <aside className="lg:w-1/5">
             {lens && <SidebarNav lens={lens} params={params} />}
           </aside>
-          <div className="flex-1 lg:max-w-2xl">
+          <div className="flex-1">
             <div className="space-y-6">{children}</div>
           </div>
         </div>
